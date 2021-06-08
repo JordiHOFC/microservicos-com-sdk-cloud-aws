6c7982bcf02ff0640f2c0605fd887ca3c23833c8
jordihofc <jordi.silva@ufv.br>
2021-06-08 09:35:16 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/microservices/aws_productcrud/AwsProductcrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.aws_productcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProductcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProductcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservices/aws_productcrud/AwsProductcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices.aws_productcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProductcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
